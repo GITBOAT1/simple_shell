d58d957fa360a70697d0e7205ba24d69b108f40f
joe <boat232001@yahoo.com>
2021-11-04 15:13:53 -0700

    simple

--- shell.c
+++ shell.c
@@ -9,11 +9,9 @@
 
 int shell(void)
 {
-	int pid1 = fork(), stlen = 0;
-	char *line;
+	int pid1 = fork(), status, statuscode;
 	size_t len = 0;
-	char *av[] = {"\n", NULL};
-	char *token;
+	char *av[] = {"\n", NULL}, *token, *line;
 
 
 	if (pid1 < 0)
@@ -22,17 +20,25 @@ int shell(void)
 	{
 		printf("#cisfun$ ");
 		getline(&line, &len, stdin);
+		if (feof(stdin))
+			exit(3);
+
 		token = strtok(line, "\n");
 		if (execve(token, av, NULL) == -1)
 		{
-
 			perror("./shell");
-
+			exit(EXIT_FAILURE);
 		}
 
 	} else
 	{
-		wait(NULL);
-
+		wait(&status);
+		if (WIFEXITED(status))
+		{
+			statuscode = WEXITSTATUS(status);
+		        if (statuscode == 3)
+				return (99);
+		}
 	}
+	return 0;
 }
